18d0ddc3862b9a48715eab9e267782fb09eab278
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-18 16:41:22 -0600

    Update welcome index spec with more robust test

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -22,17 +22,24 @@ RSpec.describe 'Welcome Index' do
       select('Tennessee', from: :state)
       click_button "Find Parks"
 
-      expect(page).to have_content("Number of Parks: 98123")
+      expect(page).to have_content("Number of Parks: 16")
     end
 
     it "can see info about each park after click" do
       select('Tennessee', from: :state)
       click_button "Find Parks"
 
-      expect(page).to have_content("Name of Park")
-      expect(page).to have_content("Description of Park")
-      expect(page).to have_content("direction of Park")
-      expect(page).to have_content("hours of Park")
+      expect(page).to have_content("Andrew Johnson National Historic Site")
+      expect(page).to have_content("Andrew Johnson's complex presidency (1865-69) illustrates the Constitution")
+      expect(page).to have_content("The GPS setting for Andrew Johnson NHS")
+      expect(page).to have_content("Wednesday: 10:00AM - 3:00PM")
+    end
+
+    it "can click on Find Parks button for each state" do
+      @us_states.each do |state|
+        select(state[0], from: :state)
+        click_button "Find Parks"
+      end
     end
   end
 

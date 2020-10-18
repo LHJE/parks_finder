b19dc2a894df05aa9502d3f6356abb7ebdbac837
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-18 14:43:42 -0600

    Add Welcome index spec

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome Index' do
+  describe 'As a visitor' do
+    before :each do
+      visit root_path
+    end
+
+    it "can select Tennessee from the form" do
+
+    end
+
+    it "can click on Find Parks button" do
+      
+    end
+
+    it "can see total number of parks found after click" do
+
+    end
+
+    it "can see info about each park after click" do
+
+    end
+  end
+
+end
